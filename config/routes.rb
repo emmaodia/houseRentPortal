a62b4b9f4bc46adc64c6e3c0ac04bdc797f0a1ce
emmaodia <odia.emma@gmail.com>
2019-09-13 18:57:36 +0100

    Fixed Config routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  
   get 'pages/home'
 
 
   resources :houses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'pages#home'
 end
