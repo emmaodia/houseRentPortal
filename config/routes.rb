01c86472a858af48d7e8b4d83d052a2607f3fa5c
emmaodia <odia.emma@gmail.com>
2019-09-16 18:30:17 +0100

    Set permissions for Landlords and Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'pages/home'
-  get 'pages/auth'
-  devise_for :landlords
+  devise_for :users, path: 'users'
+  devise_for :landlords, path: 'landlords'
+
   root 'pages#home'
 
-  #get "/pages/:page" => "pages#show"
+  get 'pages/home'
+  get 'pages/auth'
+
 
   resources :houses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
