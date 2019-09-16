15b615f8cc08a16c5c9da962387a390fc2320ce8
emmaodia <odia.emma@gmail.com>
2019-09-16 17:49:21 +0100

    Completed Landlord Authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,4 @@
 class PagesController < ApplicationController
-
-  # GET /houses
-  # GET /houses.json
-  # def index
-  #   @houses = House.all
-  # end
-  def show
-    render template: "pages/#{params[:page]}"
+  def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  devise_for :landlords
   root 'pages#home'
 
-  get "/pages/:page" => "pages#show"
-
+  #get "/pages/:page" => "pages#show"
 
   resources :houses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
